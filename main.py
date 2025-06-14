28e0e68875c20aa649607f0c349ef1445c675dc7
ghost-mann <iamaob8@protonmail.com>
2025-06-14 19:56:28 +0300

    feat: added json.dumps method that converts json data into string in main.py

--- main.py
+++ main.py
@@ -18,13 +18,15 @@ cities = [
 
 WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
 
-weather_url = f'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={WEATHER_API_KEY}'
+# loop through the cities list 
+for city in cities:
+    weather_url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={WEATHER_API_KEY}'
+    response = requests.get(weather_url)
+    data = response.json()
 
-response = requests.get(weather_url)
-
-data = response.json()
-
-if response.status_code == 200:
-    print(data)
-else:
-    print("error")
+    if response.status_code == 200:
+        print(f"Weather for {city}:")
+        # converts json into string
+        print(json.dumps(data, indent=2))
+    else:
+        print(f"Failed for {city}: {response.status_code}")
